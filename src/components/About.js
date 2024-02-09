314a11a3cbd0e345ad0e5a5257d6af183cc7872b
Gold7EXE <156623102+Gold7EXE@users.noreply.github.com>
2024-02-09 16:47:46 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -10,14 +10,14 @@ const About = () => {
         <div className="about-container">
           <div className="about-card">
             <div>
-              <h4 className="h-color">⚡ Commands</h4>
+              <h4 className="h-color">Commands</h4>
               <p className="p-color">
                 We have more than 50 commands support in this bot.
               </p>
               <hr />
             </div>
             <div>
-              <h4 className="h-color">✉️ 24/7 Support</h4>
+              <h4 className="h-color">24/7 Support</h4>
               <p className="p-color">
                 The biggest support community server for the bot available for
                 you anytime.
@@ -25,9 +25,9 @@ const About = () => {
               <hr />
             </div>
             <div>
-              <h4 className="h-color">✏️ Customization</h4>
+              <h4 className="h-color">Customization</h4>
               <p className="p-color">
-                Fully customizable discord bot completely FREE!
+                Fully customizable discord bot, completely FREE!
               </p>
               <hr />
             </div>
@@ -41,15 +41,15 @@ const About = () => {
 
         <section className="about-stats">
           <div className="about-stats-card">
-            <h4 className="h-color">17k+</h4>
+            <h4 className="h-color">0.01k+</h4>
             <p className="p-color">Total Servers</p>
           </div>
           <div className="about-stats-card">
-            <h4 className="h-color">94k+</h4>
+            <h4 className="h-color">0.01k+</h4>
             <p className="p-color">Total Users</p>
           </div>
           <div className="about-stats-card">
-            <h4 className="h-color">375k+</h4>
+            <h4 className="h-color">1.4k+</h4>
             <p className="p-color">Total Commands Ran</p>
           </div>
         </section>
