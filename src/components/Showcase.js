139a750601b8e993d97b4662bd60a09812b3323c
Gold7EXE <156623102+Gold7EXE@users.noreply.github.com>
2024-02-10 12:45:00 -0500

    Update Showcase.js

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -22,13 +22,13 @@ const Showcase = () => {
         <span className="p-color">Bot is online!</span>
         <div className="showcase-btn">
           <button className="btn">
-            <Link className="h-color no-decoration" href="https://discord.com/oauth2/authorize?client_id=1205638151855407114&permissions=8&scope=bot+applications.commands">
+            <Link className="h-color no-decoration" href="https://discord.com/api/oauth2/authorize?client_id=1204476384165236796&permissions=8&scope=bot+applications.commands">
               Invite now!
             </Link>
           </button>
 
           <button className="btn">
-            <Link className="h-color no-decoration" href="https://discord.gg/rPEZQkXvfd">
+            <Link className="h-color no-decoration" href="https://discord.gg/overtimee">
               Join server!
             </Link>
           </button>
