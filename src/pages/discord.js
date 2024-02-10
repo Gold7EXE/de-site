933f1617e5b1f97fb9236a99b300ba27758b3f51
Gold7EXE <156623102+Gold7EXE@users.noreply.github.com>
2024-02-10 14:14:19 -0500

    Create discord.js

---
+++ src/pages/discord.js
@@ -0,0 +1,17 @@
+// discord.js
+
+export async function getServerSideProps(context) {
+  // Redirect to your Discord server URL
+  context.res.writeHead(302, { Location: 'https://discord.gg/overtimee' });
+  context.res.end();
+
+  // Return an empty object as props
+  return { props: {} };
+}
+
+const DiscordPage = () => {
+  // This component will not be rendered, as the redirection happens on the server side
+  return null;
+};
+
+export default DiscordPage;
