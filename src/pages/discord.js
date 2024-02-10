fdf27657af1892c14ca8b22b615f199106a14d6e
Gold7EXE <156623102+Gold7EXE@users.noreply.github.com>
2024-02-10 14:16:59 -0500

    Update discord.js

--- src/pages/discord.js
+++ src/pages/discord.js
@@ -2,7 +2,7 @@
 
 export async function getServerSideProps(context) {
   // Redirect to your Discord server URL
-  context.res.writeHead(302, { Location: 'https://discord.gg/overtimee' });
+  context.res.writeHead(302, { Location: 'https://discord.gg/wornsets' });
   context.res.end();
 
   // Return an empty object as props
